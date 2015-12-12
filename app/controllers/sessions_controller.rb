5570fdc73a39d988527a9397d949e91f05fc2627
yuyueugene84 <yuyueugene84@gmail.com>
2015-12-12 13:21:27 +0800

    added bycrpt gem for user authenication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        flash[:notice] = "You've logged in!"
+        redirect_to root_path
+    else
+      redirect_to login_path
+    end
+
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ActiveRecord::Base
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice]= "You have registered"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
+
