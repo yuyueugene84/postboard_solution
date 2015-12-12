306c706208d34f39ce89dc548f8460d61882794a
yuyueugene84 <yuyueugene84@gmail.com>
2015-12-13 06:21:22 +0800

    fixing migration file error

--- db/migrate/20151209132020_add_posts_table.rb
+++ db/migrate/20151209132020_add_posts_table.rb
@@ -1,9 +1,9 @@
 class AddPostsTable < ActiveRecord::Migration
   def change
-    create_table :comments do |t|
+    create_table :posts do |t|
+      t.string  :title
       t.text    :content
       t.integer :user_id
-      t.integer :post_id
  
       t.timestamps
     end
