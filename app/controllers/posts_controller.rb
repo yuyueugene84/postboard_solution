6b93148668cfa39a1a1bc1656a3e5efbc79d7dd9
yuyueugene84 <yuyueugene84@gmail.com>
2015-12-12 10:24:42 +0800

    added post new action and form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,24 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+
+    if post.save!
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
