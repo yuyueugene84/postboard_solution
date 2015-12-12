7abfcda31cc2d2492ec5d04a8e868188876ec025
yuyueugene84 <yuyueugene84@gmail.com>
2015-12-12 11:21:11 +0800

    added comment form in post show page

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(params.require(:comment).permit(:content))
+
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render 'posts/show'
+    end
+
+  end
+end
